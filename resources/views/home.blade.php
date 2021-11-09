555ee023191287070beaa0b59d77f5f76b57d518
Thicianny <thicianny2@gmail.com>
2021-11-09 18:07:04 -0300

    Finalizando

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    
+
     <title>Teste-Backend</title>
 
 </head>
@@ -39,13 +39,19 @@
                   <td>{{ $produto->titulo }}</td>
                   <td>{{ $produto->preco }}</td>
                   <td><a href="{{ url('/edit') }}/{{$produto->id}}">Editar</a></td>
-                  <td><a href="{{ url('/delete') }}/{{$produto->id}}">Excluir</a></td>
+                  <td><a href="{{ url('/delete') }}/{{$produto->id}}" onclick="excluir()">Excluir</a></td>
                 </tr>
               @endforeach
             </tbody>
           </table>
       </div>
-    </div>
+    </div> 
+    <script type="text/javascript">
+      function excluir(){
+        alert("Deletado com sucesso!")
+      }
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+   
 </body>
 </html>
